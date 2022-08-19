b46fbf3b54c622a9ae839879faf739c0f474534a
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-19 11:35:23 +0430

    init database

---
+++ app/src/main/java/ir/dunijet/studentManager/model/local/MyDatabase.kt
@@ -0,0 +1,32 @@
+package ir.dunijet.studentManager.model.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ir.dunijet.studentManager.model.local.student.Student
+import ir.dunijet.studentManager.model.local.student.StudentDao
+
+@Database(entities = [Student::class], version = 1, exportSchema = false)
+abstract class MyDatabase:RoomDatabase() {
+    abstract val studentDao: StudentDao
+
+    @Volatile
+    private var dataBase:MyDatabase?=null
+    fun getDatabase(context: Context):MyDatabase{
+
+        synchronized(this){
+            if (dataBase==null){
+                dataBase= Room.databaseBuilder(
+                    context.applicationContext,
+                    MyDatabase::class.java,
+                    "myDatabase.db"
+                )
+                    .build()
+            }
+            return dataBase!!
+        }
+    }
+
+
+}
