b67ea2b34cbe02c2232e3b13c14967891e12d4e5
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-20 12:46:44 +0430

    init AddStudentViewModel with view model component

--- app/src/main/java/ir/dunijet/studentManager/addStudent/AddStudentViewModel.kt
+++ app/src/main/java/ir/dunijet/studentManager/addStudent/AddStudentViewModel.kt
@@ -1,12 +1,11 @@
 package ir.dunijet.studentManager.addStudent
 
+import androidx.lifecycle.ViewModel
 import io.reactivex.Completable
 import ir.dunijet.studentManager.model.MainRepository
 import ir.dunijet.studentManager.model.local.student.Student
 
-class AddStudentViewModel {
-
-    private val mainRepository=MainRepository()
+class AddStudentViewModel(private val mainRepository: MainRepository):ViewModel() {
 
     fun insertStudent(student: Student):Completable{
         return mainRepository.insertStudent(student)
