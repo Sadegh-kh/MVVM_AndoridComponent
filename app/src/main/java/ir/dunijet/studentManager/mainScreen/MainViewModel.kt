9c75e97cd9bde8842e32f58c95a8d90b614894f5
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-20 10:53:56 +0430

    init view model for activity main

--- app/src/main/java/ir/dunijet/studentManager/mainScreen/MainViewModel.kt
+++ app/src/main/java/ir/dunijet/studentManager/mainScreen/MainViewModel.kt
@@ -1,22 +1,48 @@
 package ir.dunijet.studentManager.mainScreen
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import io.reactivex.Completable
-import io.reactivex.Single
-import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.CompletableObserver
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import ir.dunijet.studentManager.model.MainRepository
 import ir.dunijet.studentManager.model.local.student.Student
-import java.util.concurrent.TimeUnit
 
-class MainViewModel {
-    private val mainRepository=MainRepository()
-    val progressBarSubject=BehaviorSubject.create<Boolean>()
-    fun getAllStudent():Single<List<Student>>{
-        progressBarSubject.onNext(true)
-        return mainRepository.getAllStudents()
-            .delay(4,TimeUnit.SECONDS)
-            .doFinally { progressBarSubject.onNext(false) }
+class MainViewModel(private val mainRepository: MainRepository):ViewModel() {
+
+    private lateinit var netDisposable:Disposable
+    private val errorData=MutableLiveData<String>()
+
+    init {
+        mainRepository.refreshData().subscribeOn(Schedulers.io())
+            .subscribe(object :CompletableObserver{
+                override fun onSubscribe(d: Disposable) {
+                    netDisposable=d
+                }
+
+                override fun onComplete() {
+                }
+
+                override fun onError(e: Throwable) {
+                    errorData.postValue(e.message ?:"unknown error")
+                }
+            })
+    }
+    fun getAllStudent():LiveData<List<Student>>{
+        return mainRepository.getAllStudent()
     }
     fun deleteStudent(id: Int):Completable{
         return mainRepository.deleteStudent(id)
+    }
+
+    fun getError():LiveData<String>{
+        return errorData
+    }
+
+    override fun onCleared() {
+        netDisposable.dispose()
+        super.onCleared()
     }
 }
