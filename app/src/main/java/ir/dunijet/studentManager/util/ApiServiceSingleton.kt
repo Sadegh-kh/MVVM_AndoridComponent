e9e62c5bb9c70b82f21940419a8b57df038f984f
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-20 11:20:17 +0430

    init api service singleton for all activities

---
+++ app/src/main/java/ir/dunijet/studentManager/util/ApiServiceSingleton.kt
@@ -0,0 +1,28 @@
+package ir.dunijet.studentManager.util
+
+import com.google.gson.GsonBuilder
+import ir.dunijet.studentManager.model.server.ApiService
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiServiceSingleton {
+    private var apiService: ApiService? = null
+        get() {
+            if (field == null) {
+
+                val gson = GsonBuilder()
+                    .setLenient()
+                    .create()
+
+                val retrofit = Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+
+                field=retrofit.create(ApiService::class.java)
+            }
+            return field
+        }
+}
