120d5f5abd1adaa2b7558331afcde0d994129b6d
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-20 12:49:28 +0430

    init View Model Factory for AddStudentViewModel constructor

--- app/src/main/java/ir/dunijet/studentManager/util/ViewModelFactory.kt
+++ app/src/main/java/ir/dunijet/studentManager/util/ViewModelFactory.kt
@@ -2,11 +2,18 @@ package ir.dunijet.studentManager.util
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import ir.dunijet.studentManager.addStudent.AddStudentViewModel
 import ir.dunijet.studentManager.mainScreen.MainViewModel
 import ir.dunijet.studentManager.model.MainRepository
 
 class MainViewModelFactory(private val mainRepository: MainRepository):ViewModelProvider.Factory{
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return MainViewModel(mainRepository) as T
+    }
+}
+
+class AddStudentViewModelFactory(private val mainRepository: MainRepository):ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return AddStudentViewModel(mainRepository) as T
     }
 }
